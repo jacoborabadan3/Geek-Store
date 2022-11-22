98c17baaec84cbe730f0c6dd917b08587d235480
jacoborabadan3 <jacobortgtrader@gmail.com>
2022-11-21 21:37:15 -0600

    Primer commit: Desafio-02

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,113 @@
+import "../../index.scss"
+import "./NavBar.scss"
+
+const NavBar = () => {
+
+    return (
+
+        <nav className="Nav">
+            <div className="contenedor nav">
+                {/* logo */}
+                <section className="nav__logo">
+                    <div className="cubo">
+                        <div className="f f-up">
+                            <div className="grids">
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                            </div>
+                        </div>
+                        <div className="f f-down">
+                            <div className="grids">
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                            </div>
+                        </div>
+                        <div className="f f-right">
+                            <div className="grids">
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                            </div>
+                        </div>
+                        <div className="f f-left">
+                            <div className="grids">
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                            </div>
+                        </div>
+                        <div className="f f-front">
+                            <div className="grids">
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                            </div>
+                        </div>
+                        <div className="f f-back">
+                            <div className="grids">
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                                <div className="grid"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <h1>Geek Store</h1>
+                </section>
+                {/* navegación */}
+                <ul className="nav__items">
+                    <li className="nav-li">
+                        <a className="nav-a" href="#">Nosotros</a>
+                    </li>
+                    <li className="nav-li">
+                        <a className="nav-a" href="#">Productos</a>
+                    </li>
+                    <li className="nav-li">
+                        <a className="nav-a" href="#">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    );
+
+};
+
+export default NavBar;
